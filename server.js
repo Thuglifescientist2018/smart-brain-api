4e5ee24ae7f4eb03502804362c35140d2c2c0062
Shashwat Dahal <thuglifescientist2018@gmail.com>
2026-01-05 11:17:57 +0545

    handle signInAuthentication() with JWT token

--- server.js
+++ server.js
@@ -25,9 +25,10 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res)=> { res.send("its workingg") })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signInAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
