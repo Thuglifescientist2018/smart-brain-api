1c3fea3bfcd21fe956d155b696085b2dff01a047
Shashwat Dahal <thuglifescientist2018@gmail.com>
2026-01-12 10:20:58 +0545

    init redis

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,8 @@
-var jwt = require('jsonwebtoken');
+const  jwt = require('jsonwebtoken');
+const redis = require('redis')
 
+//setup redis
+const redisClient = redis.createClient(process.env.REDIS_URI);
 
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
